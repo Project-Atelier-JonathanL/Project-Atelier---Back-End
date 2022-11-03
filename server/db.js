94ce34be6391d8b21bc044effd702bad9820ee2f
Jonathan Liang <Jonathanliang15@gmail.com>
2022-11-03 11:53:20 -0500

    Finished tables and importing data

--- server/db.js
+++ server/db.js
@@ -10,22 +10,46 @@ const client = new Client({
 
 client.connect()
 
-client.query('CREATE TABLE IF NOT EXISTS reviews (review_id SERIAL PRIMARY KEY, product_id INT, rating INT, summary VARCHAR(255), recommend BOOLEAN, response TEXT, body VARCHAR(255), date TEXT, reported BOOLEAN, reviewer_name VARCHAR(255), reviewer_email TEXT)',
+client.query('CREATE TABLE IF NOT EXISTS reviews (id SERIAL PRIMARY KEY, product_id INT, rating INT, date TEXT, summary VARCHAR(255), body VARCHAR(1000),recommend BOOLEAN, reported BOOLEAN, reviewer_name VARCHAR(255), reviewer_email TEXT, response TEXT, helpfulness INT)',
   (err, res) => {
     if (err) {
       console.log(err.messages)
     } else {
-      console.log(res)
+      console.log(res.rows)
     }
-    client.end()
+})
+client.query('CREATE TABLE IF NOT EXISTS reviews_photos (id SERIAL PRIMARY KEY, review_id INT, url VARCHAR(255))',
+  (err, res) => {
+    if (err) {
+      console.log(err.messages)
+    } else {
+      console.log(res.rows)
+    }
+})
+client.query('CREATE TABLE IF NOT EXISTS characteristics (id SERIAL PRIMARY KEY, product_id INT, name VARCHAR(255))',
+  (err, res) => {
+    if (err) {
+      console.log(err.messages)
+    } else {
+      console.log(res.rows)
+    }
 })
 
-client.query('CREATE TABLE IF NOT EXISTS reviews_photos (id SERIAL PRIMARY KEY)',
+client.query('CREATE TABLE IF NOT EXISTS characteristic_reviews (id SERIAL PRIMARY KEY, characteristic_id INT, review_id INT, value INT)',
   (err, res) => {
     if (err) {
       console.log(err.messages)
     } else {
-      console.log(res)
+      console.log(res.rows)
+    }
+})
+// \copy characteristic_reviews from '../../../../Users/jonathanliang/Desktop/SDCdata/characteristic_reviews.csv' csv header;
+client.query('SELECT * FROM reviews_photos WHERE id=123',
+  (err, res) => {
+    if (err) {
+      console.log(err.messages)
+    } else {
+      console.log(res.rows)
     }
     client.end()
 })
